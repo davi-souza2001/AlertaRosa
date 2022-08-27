63f9da0e200cb54c3c1a4c40efa3e2b4649a8708
davi-souza2001 <davisouza2001dv@gmail.com>
2022-08-27 12:22:37 -0300

    create the subtitle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import { Header } from '../components/Header'
+import logoTitle from '../../public/images/logoTitle.svg'
 import styles from '../styles/index.module.css'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className={styles.contentGeral}>
       <Header />
-      <h1>teste</h1>
+      <div className={styles.contentTitle}>
+        <Image src={logoTitle} width={1200} height={400} />
+      </div>
+      <div className={styles.contentSubTitle}>
+        <p>The quiz game made for devs!</p>
+      </div>
     </div>
   )
 }
