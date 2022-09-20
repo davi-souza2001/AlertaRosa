617ef4b35f5d7e0cb3e93537fc646ed86eda8b3b
davi-souza2001 <davisouza2001dv@gmail.com>
2022-09-20 14:30:07 -0300

    up

--- src/components/BoxQuestion/index.tsx
+++ src/components/BoxQuestion/index.tsx
@@ -10,9 +10,7 @@ interface boxQuestionProps {
 
 export function BoxQuestion(props: boxQuestionProps) {
 	return (
-		<div className={styles.contentGeral} onClick={props.onClick}
-			// style={{ backgroundColor: `${props.selected && '#3d2767'}` }}
-		>
+		<div className={styles.contentGeral} onClick={props.onClick}>
 			<div className={styles.contentNumberQuestion}>
 				<span>{props.numberQuestion}</span>
 			</div>
