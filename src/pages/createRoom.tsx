0193df9d2f7fe4334b2b6ae778a31e369bccaa7b
davi-souza2001 <davisouza2001dv@gmail.com>
2022-10-23 11:03:11 -0300

    up

--- src/pages/createRoom.tsx
+++ src/pages/createRoom.tsx
@@ -68,6 +68,7 @@ export default function CreateRoom() {
 						</select>
 					</form>
 					<button onClick={handleSubmitNewRoom}>Create</button>
+					<p onClick={() => alert('Algo')}>Teste</p>
 					<p>Create a room to play with your friends! Or if you prefer, play single played too!</p>
 				</div>
 			</div>
