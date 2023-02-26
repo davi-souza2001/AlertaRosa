2056ba8f298a60b8e79cf9081355717e0348ccf9
Davi Souza <davisouza2001dv@gmail.com>
2023-02-25 21:50:37 -0300

    up

--- src/service/context/RoomContext.tsx
+++ src/service/context/RoomContext.tsx
@@ -1,6 +1,8 @@
 import { createContext, } from 'react'
+import { ProviderRoom } from '../../core/ProviderRoom'
 
 import { PlayerProps, RoomProps } from '../../core/Room'
+import { RoomProvider } from '../../provider/RoomProvider'
 import UseAuth from '../hooks/useAuth'
 
 interface RoomContextProps {
@@ -12,10 +14,19 @@ const RoomContext = createContext<RoomContextProps>({
 })
 
 export function RoomProviderContext(props: any) {
-	const { setLoading } = UseAuth()
+	const { setLoading, user } = UseAuth()
+	const roomProvider = new ProviderRoom(new RoomProvider())
+
 
 	async function create(room: RoomProps): Promise<void> {
 		setLoading(true)
+		await roomProvider.create(room, {
+			email: user.email,
+			name: user.name,
+			photo: user.photo,
+			xp: user.xp,
+			score: 0
+		})
 		setLoading(false)
 	}
 
