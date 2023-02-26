f5dd9fd93b5f3f798e50c5d0d1f7fd800bf3eb4f
Davi Souza <davisouza2001dv@gmail.com>
2023-02-26 09:14:21 -0300

    up

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,14 +1,15 @@
-import Image from 'next/image'
 import React from 'react'
 
-import GoogleIcon from '../../public/logoGoogle.png'
-
-export default function Login() {
+export default function Register() {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen py-2">
 			<div className="flex flex-col w-full max-w-md p-8 bg-white rounded-lg shadow-lg">
 				<h1 className="text-2xl font-bold text-gray-800 mb-4">Login</h1>
 				<form>
+					<div className="mb-4">
+						<label htmlFor="name" className="block text-gray-800 font-semibold mb-2">Name</label>
+						<input type="name" name="name" id="name" className="w-full border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring focus:ring-indigo-200" required />
+					</div>
 					<div className="mb-4">
 						<label htmlFor="email" className="block text-gray-800 font-semibold mb-2">Email</label>
 						<input type="email" name="email" id="email" className="w-full border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring focus:ring-indigo-200" required />
@@ -17,21 +18,11 @@ export default function Login() {
 						<label htmlFor="password" className="block text-gray-800 font-semibold mb-2">Password</label>
 						<input type="password" name="password" id="password" className="w-full border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring focus:ring-indigo-200" required />
 					</div>
-					<div className='h-10'>
-						<a href="#" className="text-gray-600 cursor-pointer hover:text-gray-500">Create Account</a>
-					</div>
 					<div className="flex items-center justify-between">
-						<button type="submit" className="bg-indigo-500 text-white px-4 py-2 rounded-lg hover:bg-indigo-400 focus:outline-none focus:bg-indigo-400">Login</button>
-						<a href="#" className="text-gray-600 hover:text-gray-500">Forgot Password?</a>
-					</div>
-					<div className="my-6 h-12 w-3/4 flex items-center justify-center rounded-lg font-medium cursor-pointer bg-slate-200">
-						<Image src={GoogleIcon} alt='Icone do google' height={30} width={30} />
-						<button className='ml-5'>Login com google</button>
+						<button type="submit" className="bg-indigo-500 text-white px-4 py-2 rounded-lg hover:bg-indigo-400 focus:outline-none focus:bg-indigo-400">Register</button>
 					</div>
 				</form>
 			</div>
 		</div>
 	)
 }
-
-
