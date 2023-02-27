d0f623a046258a050cd43470da8e182281e0dc0f
Vinicius <viniciusflexa@gmail.com>
2023-02-27 18:42:52 -0300

    index main box update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,15 @@
-import Image from "next/image"
 import { HiArrowNarrowRight } from "react-icons/hi"
 import { Carousel } from 'react-responsive-carousel'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
-import GirlIcon from '../../public/girllove.svg'
-
-import { TopBar } from "../components/TopBar"
 import { BoxCategorie } from "../components/BoxCategorie"
+import MainBox from "../components/MainBox"
 
 export default function Home() {
 	return (
-		<div>
-			<TopBar />
+		<div className="pt-24">
 			<div className="h-44 w-full flex items-center justify-center">
-				<div className="h-full w-11/12 flex items-center justify-between rounded-lg bg-slate-300">
-					<Image src={GirlIcon} alt='Imagem de mulher com coração' height={180} width={180} />
-					<p className="text-left ml-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non molestias laborum quis harum magnam</p>
-				</div>
+				<MainBox />
 			</div>
 			<div className='h-20 w-full flex items-center justify-between'>
 				<p className="ml-5 font-semibold text-xl">Discover</p>
