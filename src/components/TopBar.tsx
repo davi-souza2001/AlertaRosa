f2261b0622a385e13ccccd171a9f80a2bab63099
Vinicius <viniciusflexa@gmail.com>
2023-02-27 19:04:31 -0300

    z-index max

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,7 +3,7 @@ import Logo from '../../public/testUser.jpg'
 
 export function TopBar(){
 	return (
-		<div className='h-20 w-full fixed'>
+		<div className='h-20 w-full fixed z-50'>
 			<div className='h-full w-full flex items-center font-medium text-2xl pl-5 bg-slate-50 shadow-md'>
 				<div className='flex w-[50%] gap-2'>
 					<Image src={Logo} alt='Logo do quiz' height={30} width={30} className="rounded-md"/>
