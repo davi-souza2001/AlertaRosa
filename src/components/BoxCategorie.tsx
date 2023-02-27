9851e3b70708e059ee7a217324898414bd755b61
Vinicius <viniciusflexa@gmail.com>
2023-02-27 19:05:48 -0300

    categorie box sections

--- src/components/BoxCategorie.tsx
+++ src/components/BoxCategorie.tsx
@@ -1,10 +1,15 @@
+interface Props{
+	image: string
+	content: string
+}
+
 export function BoxCategorie(){
 	return (
-		<div className="h-56 w-48 rounded-lg shadow-lg">
-			<div className="h-3/5 w-full rounded-t-lg bg-red-500">
+		<div className="h-56 w-48 rounded-lg border-slate-300">
+			<div className="h-3/5 w-full rounded-t-lg bg-indigo-500">
 				teste
 			</div>
-			<div className="h-2/5 w-full rounded-b-lg bg-slate-50">
+			<div className="h-2/5 w-full rounded-b-lg bg-slate-50 border-b-4 border-x-2 border-slate-300">
 				Alo
 			</div>
 		</div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
-import { HiArrowNarrowRight } from "react-icons/hi"
-import { Carousel } from 'react-responsive-carousel'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
-
-import { BoxCategorie } from "../components/BoxCategorie"
 import MainBox from "../components/MainBox"
+import Sections from "../components/Sections"
 
 export default function Home() {
 	return (
@@ -11,18 +7,12 @@ export default function Home() {
 			<div className="h-44 w-full flex items-center justify-center">
 				<MainBox />
 			</div>
-			<div className='h-20 w-full flex items-center justify-between'>
-				<p className="ml-5 font-semibold text-xl">Discover</p>
-				<p className="mr-5 flex items-center justify-center text-indigo-500">View all <HiArrowNarrowRight /> </p>
-			</div>
-			<div className="h-60">
-				<Carousel showArrows={false} showIndicators={false} centerMode centerSlidePercentage={55}>
-					<BoxCategorie />
-					<BoxCategorie />
-					<BoxCategorie />
-				</Carousel>
-			</div>
 
+			<Sections title="Explore" />
+
+			<Sections title="Melhores Quiz" />
+
+			<Sections title="Melhores Tópicos" />
 		</div>
 	)
 }
