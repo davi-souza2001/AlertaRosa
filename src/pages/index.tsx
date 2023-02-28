3874bcc87e92e90d92e245c672d3db3aaf0df8c2
Davi Souza <davisouza2001dv@gmail.com>
2023-02-28 14:26:14 -0300

    up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,18 +18,6 @@ export default function Home() {
 
 				<Sections title="Melhores Tópicos" />
 			</div>
-			<div className='h-20 w-full flex items-center justify-between'>
-				<p className="ml-5 font-semibold text-xl">Discover</p>
-				<p className="mr-5 flex items-center justify-center text-indigo-500">View all <HiArrowNarrowRight /> </p>
-			</div>
-			<div className="h-60">
-				<Carousel showArrows={false} showIndicators={false} centerMode centerSlidePercentage={55}>
-					<BoxCategorie />
-					<BoxCategorie />
-					<BoxCategorie />
-				</Carousel>
-			</div>
-
-		</div>
+		</>
 	)
 }
