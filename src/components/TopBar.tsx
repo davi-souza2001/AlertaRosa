1170bc9f1fe069d448bd56a517c3d9f9f002f284
Davi Souza <davisouza2001dv@gmail.com>
2023-02-28 16:51:04 -0300

    up

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,9 +1,21 @@
+import { useState } from 'react'
 import Image from 'next/image'
 import Router from 'next/router'
 import { HiMenu } from 'react-icons/hi'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+
 import Logo from '../../public/logotipo.png'
 
 export function TopBar() {
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event: any) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 	return (
 		<div className='h-20 w-full flex items-center justify-between'>
 			<div className='h-full w-1/2 flex items-center font-medium text-2xl pl-5 bg-slate-50 shadow-md'>
@@ -13,7 +25,20 @@ export function TopBar() {
 				</div>
 			</div>
 			<div className='h-full w-1/2 flex items-center justify-end text-4xl'>
-				<HiMenu className='mr-2'/>
+				<HiMenu className='mr-2' onClick={(e) => handleClick(e)} />
+				<Menu
+					id="basic-menu"
+					anchorEl={anchorEl}
+					open={open}
+					onClose={handleClose}
+					MenuListProps={{
+						'aria-labelledby': 'basic-button',
+					}}
+				>
+					<MenuItem onClick={handleClose}>Profile</MenuItem>
+					<MenuItem onClick={handleClose}>My account</MenuItem>
+					<MenuItem onClick={handleClose}>Logout</MenuItem>
+				</Menu>
 			</div>
 		</div>
 	)
