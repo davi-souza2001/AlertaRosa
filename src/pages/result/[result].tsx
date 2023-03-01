d602b00af2ed93c4179445f0154ab39dfd43084c
Davi Souza <davisouza2001dv@gmail.com>
2023-03-01 16:12:43 -0300

    up

--- src/pages/result/[result].tsx
+++ src/pages/result/[result].tsx
@@ -5,14 +5,14 @@ import { TopBar } from "../../components/TopBar"
 import GirlIcon from '../../../public/girl_diary.svg'
 import { Check } from "phosphor-react"
 
-export default function StartQuestions() {
+export default function Result() {
 	return (
 		<div>
 			<TopBar/>
 			<div className="h-full w-full flex flex-col items-center justify-center p-2 gap-3">
 				<div className="w-full flex flex-col items-center justify-center text-2xl font-semibold mt-[10vh]">
 					<Image src={GirlIcon} alt='Imagem de mulher com coração' height={180} width={180} />
-					<h1 className="text-indigo-500 mt-5">Bem-vinda ao Formulário!</h1>
+					<h1 className="text-indigo-500 mt-5">Seu resultado!</h1>
 				</div>
 				<div className="w-full flex items-center justify-center text-center">
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima magni dolorum harum culpa voluptatum doloribus quis ex libero pariatur ipsam dolore, quod incidunt eius autem id, alias mollitia explicabo magnam.</p>
