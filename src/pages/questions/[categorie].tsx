144c8c16109396c53ecf8cc889c48609d494d044
Davi Souza <davisouza2001dv@gmail.com>
2023-03-04 15:34:45 -0300

    up

--- src/pages/questions/[categorie].tsx
+++ src/pages/questions/[categorie].tsx
@@ -11,6 +11,7 @@ import { QuestionProps } from "../../core/Question"
 export default function Question() {
 	const [checked, setChecked] = useState<'yes' | 'no' | ''>('')
 	const [questions, setQuestions] = useState<QuestionProps[]>([])
+	const [questionNumber, setQuestionNumber] = useState(0)
 	const categorie = useRouter().query.categorie
 	const roomProvider = new ProviderRoom(new RoomProvider())
 
@@ -19,6 +20,18 @@ export default function Question() {
 			const list = await roomProvider.getQuestions(categorie as string)
 			setQuestions(list)
 		}
+	}
+
+	function renderQuestions() {
+		return (
+			<div className="h-52 w-full flex items-center justify-center text-center p-5 text-lg font-semibold">
+				<p>{questions[questionNumber]?.enunciation}</p>
+			</div>
+		)
+	}
+
+	function nextQuestion() {
+		setQuestionNumber(state => state + 1)
 	}
 
 	useEffect(() => {
@@ -33,9 +46,7 @@ export default function Question() {
 					<Progress colorScheme='green' size='md' value={20} className="w-full ml-2 rounded-lg" />
 					<p className="p-2">1/5</p>
 				</div>
-				<div className="h-52 w-full flex items-center justify-center text-center p-5 text-lg font-semibold">
-					<p>{questions[0]?.categorie}</p>
-				</div>
+				{renderQuestions()}
 				<div className="h-44 w-full flex items-center justify-center flex-col text-center p-5 text-white">
 					<div onClick={() => setChecked('yes')} className={`h-12 w-2/3 flex items-center justify-start rounded-md mb-7 ${checked === 'yes' ? 'bg-green-500 shadow-md font-semibold' : 'bg-indigo-500'} transition-all`}>
 						{checked === 'yes' ? <HiCheckCircle className="text-white text-3xl mx-2 mr-5" /> : <p className="h-6 w-6 mx-2 mr-5 bg-white text-indigo-500 rounded-full">A</p>}
@@ -47,7 +58,7 @@ export default function Question() {
 					</div>
 				</div>
 				<div className="w-full flex items-center justify-center">
-					<button disabled={checked === '' ? true : false} className={`text-white h-10 w-32 bg-indigo-500 rounded-md opacity-50 ${checked !== '' && 'opacity-100 shadow-md'} transition-all`}>
+					<button onClick={() => nextQuestion()} disabled={checked === '' ? true : false} className={`text-white h-10 w-32 bg-indigo-500 rounded-md opacity-50 ${checked !== '' && 'opacity-100 shadow-md'} transition-all`}>
 						Continuar
 					</button>
 				</div>
