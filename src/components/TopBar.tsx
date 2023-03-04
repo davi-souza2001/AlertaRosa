df2df5d7c2e94dec81b0f09fe7e1b485b30baffd
Davi Souza <davisouza2001dv@gmail.com>
2023-03-04 17:09:14 -0300

    up

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import Image from 'next/image'
 import Router from 'next/router'
 import { HiMenu } from 'react-icons/hi'
+import { Button, Menu, MenuButton, MenuList } from '@chakra-ui/react'
 
 import Logo from '../../public/logosimple.svg'
-import { Button, Menu, MenuButton, MenuList } from '@chakra-ui/react'
 
 export function TopBar() {
 	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -14,18 +14,20 @@ export function TopBar() {
 
 	return (
 		<div className='h-20 w-full flex items-center justify-between'>
-			<div className='h-full w-full flex items-center font-medium text-xl px-5 bg-white border-b-4 border-indigo-500 shadow-md'>
+			<div className='h-full w-full flex items-center font-medium text-xl px-5 bg-roxo'>
 				<div className='flex w-[70%] gap-2 cursor-pointer' onClick={() => Router.push('/')}>
-					<Image src={Logo} alt='Logo do quiz' height={30} width={30}/>
+					<Image src={Logo} alt='Logo do quiz' height={30} width={30} />
 					<div className='flex items-end'>
-						<p>Alerta</p>
-						<p className='text-base font-light text-pink-600'>Rosa</p>
+						<p className='text-white'>Alerta</p>
+						<p className='text-base font-medium text-rosa'>Rosa</p>
 					</div>
 				</div>
 
 				<div className='h-full w-[30%] flex items-center justify-end'>
 					<Menu>
-						<MenuButton as={Button} rightIcon={<HiMenu className='mr-2 text-2xl' onClick={(e) => handleClick(e)} />} />
+						<MenuButton >
+							<HiMenu className='h-8 w-14 mr-2 text-xl text-white' onClick={(e) => handleClick(e)} />
+						</MenuButton>
 						<MenuList>
 							<div className='text-base font-normal'>
 								<div className='h-5 w-full flex items-center justify-start mx-5 my-5 cursor-pointer'>
