70c7a39530863eee06a18afe62e615c56b528c99
Vinicius <viniciusflexa@gmail.com>
2023-03-05 01:17:45 -0300

    border removed and added animation opacity

--- src/components/BoxCategorie.tsx
+++ src/components/BoxCategorie.tsx
@@ -7,11 +7,11 @@ interface Props {
 
 export function BoxCategorie() {
 	return (
-		<div className="h-56 w-48 rounded-lg border-slate-300" onClick={() => Router.push('/startQuestions/test')}>
-			<div className="h-3/5 w-full rounded-t-lg bg-[#DD61E8]">
+		<div className="h-56 w-48 rounded-lg border-slate-300 cursor-pointer lg:hover:opacity-100 lg:opacity-95 transition-opacity" onClick={() => Router.push('/startQuestions/test')}>
+			<div className="h-3/5 w-full rounded-t-lg bg-rosa">
 				teste
 			</div>
-			<div className="h-2/5 w-full rounded-b-lg bg-slate-50 border-b-4 border-x-2 border-slate-300">
+			<div className="h-2/5 w-full rounded-b-lg bg-slate-50">
 				Alo
 			</div>
 		</div>
