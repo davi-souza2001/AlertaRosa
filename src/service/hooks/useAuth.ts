6cd29d52ab9f60e7173d2573e5bb4e8ed6434e80
Davi Souza <davisouza2001dv@gmail.com>
2023-03-08 20:34:43 -0300

    up

--- src/service/hooks/useAuth.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react"
-import AuthContext from "../context/AuthContext"
-
-const UseAuth = () => useContext(AuthContext)
-
-export default UseAuth
--- src/service/hooks/useRoom.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react"
-import RoomContext from "../context/RoomContext"
-
-const UseRoom = () => useContext(RoomContext)
-
-export default UseRoom
