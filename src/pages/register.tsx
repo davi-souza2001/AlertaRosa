9bca3e29d8a9756035586325f24123c5e4be4c49
Davi Souza <davisouza2001dv@gmail.com>
2023-03-16 19:52:54 -0300

    up

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,12 +1,11 @@
 import { useToast } from '@chakra-ui/react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Key, Phone, Share, User, } from 'phosphor-react'
+import { Key, Phone, User, } from 'phosphor-react'
 import React, { FormEvent, useState } from 'react'
 import { HiMailOpen } from 'react-icons/hi'
 
 import Logotipo from '../../public/logotipo.svg'
-import BottomSm from '../components/BottomSmall'
 import GradientLg from '../components/GradientLarge'
 import Input from '../components/Input'
 import UseAuth from '../service/hooks/useAuth'
@@ -19,9 +18,18 @@ export default function Register() {
 	const toast = useToast()
 	const { createUserPassword } = UseAuth()
 
-	async function test(e: FormEvent) {
+	async function handleCreateSubmit(e: FormEvent) {
 		e.preventDefault()
-		if (name !== '' && password !== '' && phone.toString().length <= 11) {
+		if (!email.includes('@')) {
+			toast({
+				position: 'top-right',
+				title: 'Algo deu errado!',
+				description: 'Digite um email válido!',
+				status: 'error',
+				duration: 3000,
+				isClosable: true,
+			})
+		} else if (name !== '' && password !== '' && phone.toString().length <= 11) {
 			await createUserPassword(name, phone, email, password)
 		} else {
 			toast({
@@ -40,40 +48,28 @@ export default function Register() {
 			<GradientLg flex='col' gap='10'>
 				<Image src={Logotipo} alt="Logotipo" width={150} />
 
-				<p className='font-semibold text-xl text-center'>CADASTRO</p>
+				<p className='font-semibold text-xl text-center'>REGISTRE-SE</p>
 
-				<form className='flex flex-col gap-10'>
+				<form className='flex flex-col p-2 mt-[-30px]'>
 					<div className='flex flex-col gap-2'>
-						<div className='flex flex-col gap-5'>
-							<Input type="text" value={name} valueChange={setName} icon={<User />} placeholder="Nome" />
+						<div className='flex flex-col gap-10'>
+							<Input type="text" value={name} valueChange={setName} icon={<User />} placeholder="Name" />
 							<Input type="text" value={email} valueChange={setEmail} icon={<HiMailOpen />} placeholder="Email" />
-							<Input type="number" value={phone !== 0 ? phone : null} valueChange={setPhone} icon={<Phone />} placeholder="Telefone" />
+							<Input type="number" value={phone === 0 ? null : phone} valueChange={setPhone} icon={<Phone />} placeholder="Telefone" />
 							<Input type="password" value={password} valueChange={setPassword} icon={<Key />} placeholder="Senha" />
+						</div>
+
+						<div className='flex underline text-xs lg:text-sm justify-between'>
+							<Link href="/login">Esqueci minha senha</Link>
+							<Link href="/register">Criar conta</Link>
 						</div>
 					</div>
 
-					<button
-						onClick={(e) => test(e)}
-						className='bg-white text-rosa p-2 text-xl rounded-lg shadow-md lg:hover:opacity-90 transition-opacity'
-					>
-						CADASTRAR
+					<button onClick={handleCreateSubmit} className='bg-white text-rosa p-2 mt-2 text-xl rounded-lg shadow-md lg:hover:opacity-90 transition-opacity'>
+						CRIAR
 					</button>
 				</form>
 			</GradientLg>
-
-			<BottomSm items='center'>
-				<Link href="/login">
-					<div className='flex w-80 mt-20 lg:hover:opacity-90 transition-opacity'>
-						<div className='bg-white text-roxo border-2 border-roxo rounded-l-lg text-xl p-2 py-4'>
-							<Share />
-						</div>
-
-						<div className='w-full text-center bg-gradient-to-r from-roxo to-rosa rounded-r-lg p-2 py-4'>
-							JÁ TENHO CONTA
-						</div>
-					</div>
-				</Link>
-			</BottomSm>
 		</div>
 	)
 }
